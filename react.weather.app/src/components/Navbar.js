a17866384494d042a30797a7d2de06a20e0445b6
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-02-02 11:11:08 -0700

    added all necessary packages for project

---
+++ react.weather.app/src/components/Navbar.js
@@ -0,0 +1,8 @@
+export default function Navbar () {
+
+    return (
+        <div className="fixed top-0 text-center">
+            <p>Weather App</p>
+        </div>
+    )
+}
---
+++ react.weather.app/src/components/PageContainer.js
@@ -0,0 +1,7 @@
+import NavBar from "./Navbar"
+
+export default function Page() {
+  return <div className="flex bg">
+        <NavBar />
+  </div>
+}
