e0614e5ce7bde5f94c7ceac15f621f99179dcf9e
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-02-02 12:03:20 -0700

    sandboxing with tailwind

--- react.weather.app/src/components/Navbar.js
+++ react.weather.app/src/components/Navbar.js
@@ -1,6 +1,8 @@
 export default function NavBar () {
 
     return (
-        <p className="text-fuchsia-600">Weather App</p>
+      <div className="flex fixed top-0 bg-black w-screen h-16 justify-center items-center">
+        <p className="text-fuchsia-600 text-4xl">Weather App</p>
+      </div>
     );
 }
