be4910ba3cf5e0c27de5bd58d9606d48b14bf108
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-02-02 20:10:54 -0700

    created a basic scene, with sky backgroud and a few 3D text models

--- react.weather.app/src/components/PageContainer.js
+++ react.weather.app/src/components/PageContainer.js
@@ -1,7 +1,12 @@
 import NavBar from "./Navbar"
+import Scene from "./Scene"
+import { Canvas } from "@react-three/fiber";
+import * as THREE from "three";
 
 export default function Page() {
-  return <div>
-        <NavBar />
-  </div>
+  return (
+    <div>
+      <Scene />
+    </div>
+  );
 }
