2cf20d897b13aafad8ff150d493a6ee4b7601996
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-02-02 21:48:51 -0700

    styled the text of the landing display, styled the world

--- react.weather.app/src/components/Scene.js
+++ react.weather.app/src/components/Scene.js
@@ -1,58 +1,79 @@
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
-import { CameraControls, PerspectiveCamera, Text3D, Center, OrbitControls, OrthographicCamera, Sky } from "@react-three/drei";
-
-
+import {
+  CameraControls,
+  PerspectiveCamera,
+  Text3D,
+  Center,
+  OrbitControls,
+  OrthographicCamera,
+  Environment,
+  GradientTexture
+} from "@react-three/drei";
 
 export default function Scene() {
-
   return (
     <Canvas style={{ width: "100vw", height: "100vh" }}>
-      <ambientLight intensity={0.1} />
-      <directionalLight position={[10, 10, 10]} />
-      <Sky
-        distance={450000}
-        sunPosition={[0, 1, 0]}
-        inclination={0}
-        azimuth={0.25}
-      />
+      <color attach="background" args={["#151520"]} />
+      {/* <Sky sunPosition={[150, 80, 150]} /> */}
       <CameraControls />
-      <OrbitControls
-        enableZoom={false}
-        enablePan={false}
-        minPolarAngle={Math.PI / 2}
-        maxPolarAngle={Math.PI / 2}
-      />
-      <OrthographicCamera position={[0, 0, -7]}>
+      <OrthographicCamera position={[0, 3, -11]}>
         {/* Weather App Text */}
-        <Center rotation={[1, 0, 0]} position={[0, 4, 0]}>
+        <Center rotation={[0.8, 0, 0]} position={[0, 5.5, 0]}>
           <Text3D
             curveSegments={32}
             bevelEnabled
             bevelSize={0.04}
-            bevelThickness={0.001}
-            height={0.5}
+            bevelThickness={0.01}
+            height={1}
             lineHeight={0}
             letterSpacing={-0.06}
-            size={4.5}
+            size={3.5}
             font="/Roboto_Regular.json"
           >
-            Weather?
-            <meshBasicMaterial color="#00bfff" />
+            WEATHER
+            <meshBasicMaterial>
+              <GradientTexture
+                stops={[0, 1]}
+                colors={["aquamarine", "hotpink"]} // Colors need to match the number of stops
+              />
+            </meshBasicMaterial>
           </Text3D>
         </Center>
+        {/* Local Forecast Text */}
         <Center>
-          <Text3D letterSpacing={-0.06} size={2} font="/Roboto_Regular.json">
+          <Text3D
+            curveSegments={32}
+            bevelEnabled
+            bevelSize={0.04}
+            bevelThickness={0.01}
+            height={0.6}
+            lineHeight={0}
+            letterSpacing={-0.06}
+            size={2.2}
+            font="/Roboto_Regular.json"
+          >
             Local Forecast
+            <meshBasicMaterial>
+              <GradientTexture
+                stops={[0, 1]}
+                colors={["aquamarine", "hotpink"]} // Colors need to match the number of stops
+              />
+            </meshBasicMaterial>
           </Text3D>
         </Center>
         {/* Search Text */}
-        <Center position={[-20, 1, 30]} rotation={[0, 8, 0]}>
+        <Center position={[-10, 0, 15]} rotation={[0, 8, 0]}>
           <Text3D letterSpacing={-0.06} size={2} font="/Roboto_Regular.json">
             Search
             <meshBasicMaterial color="#ADD8E6" />
           </Text3D>
         </Center>
       </OrthographicCamera>
+      <OrbitControls makeDefault />
+      <Environment
+        files="https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/syferfontein_0d_clear_puresky_1k.hdr"
+        ground={{ height: 45, radius: 100, scale: 300 }}
+      />
     </Canvas>
   );
 }
