88a427b819926e84bdd7bba770afa9428d2290b8
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-02-03 22:37:26 -0700

    moved cloud and sun models down

--- react.weather.app/src/components/SearchText.js
+++ react.weather.app/src/components/SearchText.js
@@ -10,22 +10,41 @@ export default function Search () {
     }
 
     return (
-      <Center position={[-8.5, 3, 5]} rotation={[0.2, 1, 0]}>
-        <Text3D
-          onClick={searchBar}
-          curveSegments={32}
-          bevelEnabled
-          bevelSize={0.04}
-          bevelThickness={0.01}
-          height={0.4}
-          lineHeight={0}
-          letterSpacing={-0.06}
-          size={2}
-          font="/Roboto_Regular.json"
-        >
-          Search
-          <meshBasicMaterial color="#ADD8E6" />
-        </Text3D>
-      </Center>
+      <mesh>
+        <Center position={[-8.5, 2, 5]} rotation={[0.2, 1, 0]}>
+          <Text3D
+            onClick={searchBar}
+            curveSegments={32}
+            bevelEnabled
+            bevelSize={0.04}
+            bevelThickness={0.01}
+            height={0.4}
+            lineHeight={0}
+            letterSpacing={-0.06}
+            size={2}
+            font="/Roboto_Regular.json"
+          >
+            Click To
+            <meshBasicMaterial color="#ADD8E6" />
+          </Text3D>
+        </Center>
+        <Center position={[-8.5, -1, 5]} rotation={[0.2, 1, 0]}>
+          <Text3D
+            onClick={searchBar}
+            curveSegments={32}
+            bevelEnabled
+            bevelSize={0.04}
+            bevelThickness={0.01}
+            height={0.4}
+            lineHeight={0}
+            letterSpacing={-0.06}
+            size={2}
+            font="/Roboto_Regular.json"
+          >
+            Search
+            <meshBasicMaterial color="#ADD8E6" />
+          </Text3D>
+        </Center>
+      </mesh>
     );
 }
