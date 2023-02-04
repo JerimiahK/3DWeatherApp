80af4d0cb41b90132db8439b4c5216b5f6651e90
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-02-03 22:23:10 -0700

    cleaned up code, and componentized more of the scene for a cleaner canvas

--- react.weather.app/src/components/SunModel.js
+++ react.weather.app/src/components/SunModel.js
@@ -4,10 +4,9 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { useGLTF, useAnimations } from "@react-three/drei";
 
 export default function SunModel() {
-
-    // const { scene, animations } = useGLTF(url);
-    // const { actions } = useAnimations(animations, scene);
+  // const { scene, animations } = useGLTF(url);
+  // const { actions } = useAnimations(animations, scene);
 
   const gltf = useLoader(GLTFLoader, Sun);
-  return <primitive object={gltf.scene}/>;
+  return <primitive object={gltf.scene} />;
 }
