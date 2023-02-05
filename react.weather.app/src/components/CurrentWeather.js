600d115b089e63625bd468022f39e909c6667f3a
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-02-05 14:56:14 -0700

    fixed dynamically displaying current weather data

--- react.weather.app/src/components/CurrentWeather.js
+++ react.weather.app/src/components/CurrentWeather.js
@@ -4,27 +4,27 @@ import skyVideo from "../videos/skyVideo.mov";
 
 export default function GraphDisplay({ userSearch }) {
   const key = "a9fab27ee99c4f64b3c174452230502";
-  const apiURL = `http://api.weatherapi.com/v1/current.json?key=${key}&q=${userSearch}`;
-  const [url, setUrl] = useState(apiURL);
-  const [data, setData] = useState(); 
+  let apiURL = `http://api.weatherapi.com/v1/current.json?key=${key}&q=${userSearch}`;
+  const [data, setData] = useState();
 
-  console.log(url);
+  console.log(apiURL + " line 10");
 
   useEffect(() => {
     async function getData() {
-      const currentData = await fetch(url, {
+      const currentData = await fetch(apiURL, {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
           Accept: "application/json",
         },
       }).then((res) => res.json());
-      console.log(currentData);
-      setData(currentData)
-      setUrl(url)
+      console.log(currentData.current);
+      setData(currentData.current);
     }
     getData();
-  }, [url]);
+  }, [apiURL]);
+
+  console.log(data);
 
   const [video] = useState(() => {
     const vid = document.createElement("video");
@@ -43,15 +43,17 @@ export default function GraphDisplay({ userSearch }) {
         Current Weather
       </Text>
       <Text scale={0.15} color="black" position={[0, 0.3, 0.01]}></Text>
-      <Text scale={0.2} color="black" position={[0.6, 0, 0.01]}></Text>
+      <Text scale={0.2} color="black" position={[0.6, 0, 0.01]}>
+
+      </Text>
       <Text scale={0.2} color="black" position={[-0.2, 0, 0.01]}>
-        Temp: °F
+        Temp: {data ? data.temp_f : ""}°F
       </Text>
       <Text scale={0.2} color="black" position={[-0.18, -0.3, 0.01]}>
-        Wind: mph
+        Wind: {data ? data.wind_mph : ""}mph
       </Text>
       <Text scale={0.2} color="black" position={[-0.17, -0.6, 0.01]}>
-        Humidity: %
+        Humidity: {data ? data.humidity : ""}%
       </Text>
       <meshBasicMaterial color="aquamarine">
         <videoTexture attach="map" args={[video]} />
