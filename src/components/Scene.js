b261c3b9f8d281ce450983a04281eee450682504
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-02-06 10:01:53 -0700

    fixing deployment bug

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,6 +1,6 @@
 import React, { useState, lazy } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Environment, Loader } from "@react-three/drei";
+import { OrbitControls, Environment } from "@react-three/drei";
 import { Html } from "@react-three/drei";
 // Model Imports
 import Moon from "./MoonModel";
